ff661230f654ae79d241c6cba68b7e4ad7230869
NicolasAguilar-prog <naguilarurgiles@gmail.com>
2026-05-05 09:50:49 -0500

    UPD: SHELL

--- src/App.java
+++ src/App.java
@@ -6,30 +6,48 @@ public class App {
         Scanner entrada = new Scanner(System.in);
 
         int[] arreglo = {12, -7, 25, 0, -15, 33, 19, -22, 5, 48, -3};
-        int[] arregloInsercion = arreglo.clone();
-        int[] arregloShell = Arrays.copyOf(arreglo, arreglo.length);
 
         Insercion insercion = new Insercion();
         Shell shell = new Shell();
-    
+
         int opcionMenu;
 
-        do{
+        do {
             System.out.println("\n==== PROGRAMA DE ORDENAMIENTO ====");
             System.out.println("1. Ejecutar ordenamientos");
             System.out.println("2. Salir");
             System.out.print("Ingrese una opcion --> ");
             opcionMenu = entrada.nextInt();
 
-            switch (opcionMenu){
+            switch (opcionMenu) {
                 case 1:
+                    int[] arregloInsercion = arreglo.clone();
+                    int[] arregloShell = Arrays.copyOf(arreglo, arreglo.length);
+
                     System.out.println("\nArreglo original: " + Arrays.toString(arreglo));
-                    
-                    insercion.sort(arregloInsercion, true);
-                    System.out.println("Arreglo ordenado con Insercion (ascendente): " + Arrays.toString(arregloInsercion));
-                    
-                    //shell.sort(arregloShell, true);
-                    System.out.println("Arreglo ordenado con Shell (ascendente): " + Arrays.toString(arregloShell));
+
+                    System.out.println("\nInsercion:");
+                    System.out.println("1. Ascendente");
+                    System.out.println("2. Descendente");
+                    System.out.print("Elija tipo --> ");
+                    int opInsercion = entrada.nextInt();
+
+                    boolean ascInsercion = (opInsercion == 1);
+
+                    insercion.sort(arregloInsercion, ascInsercion);
+                    System.out.println("Arreglo ordenado con Insercion: " + Arrays.toString(arregloInsercion));
+
+                    System.out.println("\nShell:");
+                    System.out.println("1. Ascendente");
+                    System.out.println("2. Descendente");
+                    System.out.print("Elija tipo --> ");
+                    int opShell = entrada.nextInt();
+
+                    boolean ascShell = (opShell == 1);
+
+                    shell.sort(arregloShell, ascShell);
+                    System.out.println("Arreglo ordenado con Shell: " + Arrays.toString(arregloShell));
+
                     break;
 
                 case 2:
@@ -40,8 +58,6 @@ public class App {
                     System.out.println("Opcion incorrecta");
             }
 
-        }while(opcionMenu!=2);
-
+        } while (opcionMenu != 2);
     }
-
 }
--- src/Shell.java
+++ src/Shell.java
@@ -1,3 +1,83 @@
 public class Shell {
-    
+
+    public void sort(int[] array, boolean asc) {
+        int comparaciones = 0;
+        int cambios = 0;
+        int iteraciones = 0;
+
+        System.out.println("\n==== METODO SHELL ====");
+        System.out.println("Arreglo original:");
+        printArreglo(array);
+        System.out.println();
+
+        int n = array.length;
+
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+
+            for (int i = gap; i < n; i++) {
+                iteraciones++;
+
+                int temp = array[i];
+                int j = i;
+                boolean huboCambio = false;
+
+                while (j >= gap) {
+                    comparaciones++;
+
+                    int a = j - gap;
+                    int b = j;
+
+                    boolean debeMoverse;
+
+                    if (asc) {
+                        debeMoverse = array[j - gap] > temp;
+                    } else {
+                        debeMoverse = array[j - gap] < temp;
+                    }
+
+                    String cambioSiNo = debeMoverse ? "cambio=si" : "cambio=no";
+
+                    System.out.println("I" + i + ":\t" +
+                            "gap=" + gap + "\t" +
+                            "a=" + a + "\t" +
+                            "b=" + b + "\t" +
+                            "[a]=" + array[a] + "\t" +
+                            "[b]=" + array[b] + "\t" +
+                            cambioSiNo);
+
+                    if (debeMoverse) {
+                        array[j] = array[j - gap];
+                        cambios++;
+                        huboCambio = true;
+                        j -= gap;
+                    } else {
+                        break;
+                    }
+                }
+
+                array[j] = temp;
+
+                if (huboCambio) {
+                    System.out.println();
+                    printArreglo(array);
+                }
+
+                System.out.println();
+            }
+        }
+
+        System.out.println("END");
+        printArreglo(array);
+        System.out.println("\nCOMPARACIONES: " + comparaciones);
+        System.out.println("ITERACIONES: " + iteraciones);
+        System.out.println("CAMBIOS: " + cambios);
+        System.out.println();
+    }
+
+    private void printArreglo(int[] array) {
+        for (int valor : array) {
+            System.out.printf("%-8d", valor);
+        }
+        System.out.println();
+    }
 }
